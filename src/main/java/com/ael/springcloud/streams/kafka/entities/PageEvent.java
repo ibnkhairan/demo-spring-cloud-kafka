228467205524a2d58f1bd4b48bcfbb764eb283e8
Abdeljalil <elmarradi@hotmail.fr>
2024-01-09 13:56:43 +0100

    first commit

---
+++ src/main/java/com/ael/springcloud/streams/kafka/entities/PageEvent.java
@@ -0,0 +1,4 @@
+package com.ael.springcloud.streams.kafka.entities;
+
+public class PageEvent {
+}
---
+++ src/test/java/com/ael/springcloud/streams/kafka/DemoSpringCloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ael.springcloud.streams.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCloudKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
